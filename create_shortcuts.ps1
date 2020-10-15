bd7f73cdb578088c54d5d7d036e407f0f050c2c9
Mickael MASQUELIN <mickael.masquelin@univ-lille.fr>
2020-10-15 17:16:10 +0200

    Added sample PS script to download and install Mozilla Firefox

--- create_shortcuts.ps1
+++ create_shortcuts.ps1
@@ -1,13 +1,24 @@
 # Demo pour création d'un raccourci avec WinPS
-$sourceFileLocation = "$env:SystemRoot\System32\notepad.exe"
-$shortcutLocation = "$home\Desktop\Notepad.lnk"
+# $sourceFileLocation = "$env:SystemRoot\System32\notepad.exe"
+$sourceFileLocation = "$env:Programfiles\Mozilla Firefox\firefox.exe"
+
+$shortcutLocation01 = "$home\Desktop\AppSi.lnk"
+$shortcutLocation02 = "$home\Desktop\Salles CRIStAL.lnk"
 
 # New-Object : utilisé pour créer des objets .NET et COM
 # -ComObject WScript.Shell : créer un objet de type COM de type WScript.Shell pour appeler la méthode CreateShortCut
 $WScriptShell = New-Object -ComObject WScript.Shell
 
-$shortcut = $WScriptShell.CreateShortcut($shortcutLocation)
+$shortcut = $WScriptShell.CreateShortcut($shortcutLocation01)
 $shortcut.TargetPath = $sourceFileLocation
+$shortcut.Arguments = "https://appsi.lifl.fr/"
+
+# Sauvegarder le raccourci à l'emplacement défini (cf. TargetPath)
+$shortcut.Save()
+
+$shortcut = $WScriptShell.CreateShortcut($shortcutLocation02)
+$shortcut.TargetPath = $sourceFileLocation
+$shortcut.Arguments = "-kiosk https://salles.cristal.univ-lille.fr/Web/"
 
 # Sauvegarder le raccourci à l'emplacement défini (cf. TargetPath)
 $shortcut.Save()
---
+++ install_firefox.ps1
@@ -0,0 +1,31 @@
+# Installation silencieuse de Mozilla Firefox ESR (win64)
+# URL: https://download.mozilla.org/?product=firefox-esr-latest-ssl&os=win64&lang=fr
+
+# Définition d'un répertoire de travail pour le téléchargement et l'installation
+$workdir = "$home\Downloads\"
+
+# Récupérer la dernière version du webbrowser
+
+$source = "https://download.mozilla.org/?product=firefox-esr-latest-ssl&os=win64&lang=fr"
+$destination = "$workdir\firefox.exe"
+
+# Test si le cmdlet Invoke-Webrequest existe 
+# A défaut, on utilise la classe WebClient
+if (Get-Command 'Invoke-Webrequest')
+{
+     Invoke-WebRequest $source -OutFile $destination
+}
+else
+{
+    $WebClient = New-Object System.Net.WebClient
+    $webclient.DownloadFile($source, $destination)
+}
+
+# Démarrer l'installation
+Start-Process -FilePath "$workdir\firefox.exe" -ArgumentList "/S"
+
+# Attendre 35 secondes la fin de l'installation
+Start-Sleep -s 35
+
+# Supprimer le programme d'installation
+rm -Force $workdir\firefox*
