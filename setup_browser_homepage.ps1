5461117494a831ffc90745bbce4917314e969d67
Mickael MASQUELIN <mickael.masquelin@univ-lille.fr>
2020-10-16 14:28:38 +0200

    Added instructions to setup homepage in Google Chrome

--- setup_browser_homepage.ps1
+++ setup_browser_homepage.ps1
@@ -6,8 +6,28 @@ $homepageURL = 'https://www.cristal.univ-lille.fr'
 New-PSDrive -Name HKCR -PSProvider Registry -Root HKEY_CLASSES_ROOT
 
 # Configurer la page d'accueil pour IE
-Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Internet Explorer\main' -Name 'Start Page' -value $homepageURL
+Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Internet Explorer\main' -Name 'Start Page' -Value $homepageURL
 
 # Configurer la page d'accueil pour Edge (ancienne version)
 Set-ItemProperty -Path 'HKCR:\Local Settings\Software\Microsoft\Windows\CurrentVersion\AppContainer\Storage\microsoft.microsoftedge_8wekyb3d8bbwe\MicrosoftEdge\Main' -Name 'HomeButtonEnabled' -type 'DWord' -value '00000001'
 Set-ItemProperty -Path 'HKCR:\Local Settings\Software\Microsoft\Windows\CurrentVersion\AppContainer\Storage\microsoft.microsoftedge_8wekyb3d8bbwe\MicrosoftEdge\Main' -Name 'HomeButtonPage' -value 'https://www.cristal.univ-lille.fr'
+
+# Configurer la page d'accueil pour Google Chrome
+if ((Test-Path HKLM:\SOFTWARE\Policies\Google\Chrome) -eq $false) {
+    New-Item -path HKLM:\SOFTWARE\Policies\Google
+    New-Item -path HKLM:\SOFTWARE\Policies\Google\Chrome
+    New-ItemProperty -path 'HKLM:\SOFTWARE\Policies\Google\Chrome' -Name RestoreOnStartup -PropertyType Dword -Value 4
+    New-ItemProperty -path 'HKLM:\SOFTWARE\Policies\Google\Chrome' -Name HomepageLocation -PropertyType String -Value $homepageURL
+    New-ItemProperty -path 'HKLM:\SOFTWARE\Policies\Google\Chrome' -Name HomepageIsNewTabPage -PropertyType DWord -Value 0
+} else {
+    Set-ItemProperty -path 'HKLM:\SOFTWARE\Policies\Google\Chrome' -Name RestoreOnStartup -PropertyType Dword -Value 4
+    Set-ItemProperty -path 'HKLM:\SOFTWARE\Policies\Google\Chrome' -Name HomepageLocation -PropertyType String -Value $homepageURL
+    Set-ItemProperty -path 'HKLM:\SOFTWARE\Policies\Google\Chrome' -Name HomepageIsNewTabPage -PropertyType DWord -Value 0
+}
+
+if ((Test-Path HKLM:\SOFTWARE\Policies\Google\Chrome\RestoreOnStartupURLs) -eq $false) {
+    New-Item -Path HKLM:\SOFTWARE\Policies\Google\Chrome\RestoreOnStartupURLs
+    New-ItemProperty -Path 'HKLM:\SOFTWARE\Policies\Google\Chrome\RestoreOnStartupURLs' -Name 1 -PropertyType String -Value $homepageURL
+} else {
+    Set-ItemProperty -Path 'HKLM:\SOFTWARE\Policies\Google\Chrome\RestoreOnStartupURLs' -Name 1 -Value $homepageURL
+}
