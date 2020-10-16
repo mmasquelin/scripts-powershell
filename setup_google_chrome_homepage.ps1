39a7c1313392d96984a55d32c400637ae2fcb9ea
Mickael MASQUELIN <mickael.masquelin@univ-lille.fr>
2020-10-16 15:29:25 +0200

    Modified Google Chrome install script to setup homepage

--- setup_google_chrome_homepage.ps1
+++ setup_google_chrome_homepage.ps1
@@ -1,3 +1,4 @@
+# Ne fonctionne pas via clefs de registre ?
 $homepageURL = 'https://www.cristal.univ-lille.fr'
 
 # Arrête le script si l'utilisateur n'a pas le rôle d'administrateur
@@ -8,21 +9,21 @@ if (-not $myCurrentUser.IsInRole( [Security.Principal.WindowsBuiltInRole]::Admin
 }
 
 # Configurer la page d'accueil pour Google Chrome
-if ((Test-Path HKLM:\SOFTWARE\Policies\Google\Chrome) -eq $false) {
+if (Test-Path HKLM:\SOFTWARE\Policies\Google\Chrome) {
+    Set-ItemProperty -path 'HKLM:\SOFTWARE\Policies\Google\Chrome' -Name RestoreOnStartup -Type 'Dword' -Value 4
+    Set-ItemProperty -path 'HKLM:\SOFTWARE\Policies\Google\Chrome' -Name HomepageLocation -Type 'String' -Value $homepageURL
+    Set-ItemProperty -path 'HKLM:\SOFTWARE\Policies\Google\Chrome' -Name HomepageIsNewTabPage -Type 'DWord' -Value 0
+} else {
     New-Item -path HKLM:\SOFTWARE\Policies\Google
     New-Item -path HKLM:\SOFTWARE\Policies\Google\Chrome
-    New-ItemProperty -path 'HKLM:\SOFTWARE\Policies\Google\Chrome' -Name RestoreOnStartup -PropertyType Dword -Value 4
-    New-ItemProperty -path 'HKLM:\SOFTWARE\Policies\Google\Chrome' -Name HomepageLocation -PropertyType String -Value $homepageURL
-    New-ItemProperty -path 'HKLM:\SOFTWARE\Policies\Google\Chrome' -Name HomepageIsNewTabPage -PropertyType DWord -Value 0
-} else {
-    Set-ItemProperty -path 'HKLM:\SOFTWARE\Policies\Google\Chrome' -Name RestoreOnStartup -PropertyType Dword -Value 4
-    Set-ItemProperty -path 'HKLM:\SOFTWARE\Policies\Google\Chrome' -Name HomepageLocation -PropertyType String -Value $homepageURL
-    Set-ItemProperty -path 'HKLM:\SOFTWARE\Policies\Google\Chrome' -Name HomepageIsNewTabPage -PropertyType DWord -Value 0
+    New-ItemProperty -path 'HKLM:\SOFTWARE\Policies\Google\Chrome' -Name RestoreOnStartup -Type 'Dword' -Value 4
+    New-ItemProperty -path 'HKLM:\SOFTWARE\Policies\Google\Chrome' -Name HomepageLocation -Type 'String' -Value $homepageURL
+    New-ItemProperty -path 'HKLM:\SOFTWARE\Policies\Google\Chrome' -Name HomepageIsNewTabPage -Type 'DWord' -Value 0
 }
 
-if ((Test-Path HKLM:\SOFTWARE\Policies\Google\Chrome\RestoreOnStartupURLs) -eq $false) {
-    New-Item -Path HKLM:\SOFTWARE\Policies\Google\Chrome\RestoreOnStartupURLs
-    New-ItemProperty -Path 'HKLM:\SOFTWARE\Policies\Google\Chrome\RestoreOnStartupURLs' -Name 1 -PropertyType String -Value $homepageURL
-} else {
+if (Test-Path HKLM:\SOFTWARE\Policies\Google\Chrome\RestoreOnStartupURLs) {
     Set-ItemProperty -Path 'HKLM:\SOFTWARE\Policies\Google\Chrome\RestoreOnStartupURLs' -Name 1 -Value $homepageURL
+} else {
+    New-Item -Path HKLM:\SOFTWARE\Policies\Google\Chrome\RestoreOnStartupURLs
+    New-ItemProperty -Path 'HKLM:\SOFTWARE\Policies\Google\Chrome\RestoreOnStartupURLs' -Name 1 -Type 'String' -Value $homepageURL
 }
